93b2372ff06bfcd2e94b3834e8f8f1f7b6928599
chukotka-forever <agafdl@gmail.com>
2023-11-06 19:40:17 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -75,9 +75,3 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.lahaina \
-    libgptutils \
-    libz \
-    libcutils
